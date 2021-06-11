cc98e797849a072b450cc7a0941006a3df2a9497
Philip Thesen <45657867+thesenphilip@users.noreply.github.com>
2021-06-11 19:46:37 -0400

    Lab3 commit

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            ViewData["Bottles"] = Request.Form["Bottles"];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            return View("Error");
+
+        }
+
+
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+
+
+       [Required]
+       [StringLength(100)]
+        public String FirstName
+        {
+            get;
+            set;
+        }
+
+
+        [Required]
+        [StringLength(100)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public string DateofBirth
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
