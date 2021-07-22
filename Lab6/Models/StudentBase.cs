2a9db67505fddc9d2c141d9769b732a87383aa5c
Philip Thesen <45657867+thesenphilip@users.noreply.github.com>
2021-07-21 22:14:01 -0400

    lab 6 commit

---
+++ Lab6/Models/StudentBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.Models
+{
+    public class StudentBase
+    {
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string FirstName
+        {
+            get; set;
+
+        }
+
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string LastName
+        {
+            get; set;
+        }
+
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string Program
+        {
+            get; set;
+        }
+
+    }
+}
